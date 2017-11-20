bcb9e9a7faeeadb76dcd7241524b427754d068dd
Li-Xin <mzydj@139.com>
2017-11-20 16:42:48 +0800

    feature:增加绘制表格功能

--- designs.js
+++ designs.js
@@ -2,9 +2,25 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+let gridHeight,gridWidth,gridColor;
 function makeGrid() {
-
-// Your code goes here!
-
+	gridWidth=$('#input_width').val();
+	gridHeight=$('#input_height').val();
+	// console.log(gridWidth);
+	// console.log(gridHeight);
+	$('table').empty();
+	for(;gridHeight>0;gridHeight--){
+		$('table').append(`<tr class='${gridHeight}'></tr>`);
+		for(let i=gridWidth;i>0;i--){
+			$('tr').eq(-1).append(`<td class='${gridHeight}_${i}'></td>`);
+		}
+	}
 }
+
+$('#sizePicker').bind('submit',function(){
+	// console.log('点adfadfadfadf击')
+	makeGrid();
+	return false;
+});
+
+console.log($('#input_height').val())
